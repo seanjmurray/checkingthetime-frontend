ee4cc399ed8c7af0873453d80ab9b8dd665db3c4
Sean Murray <sean.j.murray@protonmail.com>
2022-03-04 00:29:02 -0800

    added hook to hadle text input for state change

--- src/views/components/form.jsx
+++ src/views/components/form.jsx
@@ -1,37 +1,95 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
+const watchObj = {
+  title: "",
+  brand: "",
+  model: "",
+  caliber: "",
+  origin: "",
+  notes: "",
+};
+
 const UploadForm = () => {
+  const [watch, setWatch] = useState(watchObj);
+
+  const handleInputs = (e) => {
+    const obj = watch;
+    obj[e.target.id] = e.target.value;
+    setWatch(obj);
+    console.log(watch);
+  };
+
   return (
     <>
       <Form>
         <Form.Group>
-          <Form.Label>Title</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Label>Title:</Form.Label>
+          <Form.Control
+            id="title"
+            required
+            type="text"
+            placeholder="My New Watch"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group></Form.Group>
         <Form.Group>
-          <Form.Label>Brand</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Label>Brand:</Form.Label>
+          <Form.Control
+            id="brand"
+            required
+            type="text"
+            placeholder="Seiko"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Model</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Label>Model:</Form.Label>
+          <Form.Control
+            id="model"
+            required
+            type="text"
+            placeholder="5 Sport"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Caliber</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Label>Caliber:</Form.Label>
+          <Form.Control
+            id="caliber"
+            required
+            type="text"
+            placeholder="4R36"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Origin</Form.Label>
-          <Form.Control type="text"></Form.Control>
+          <Form.Label>Origin:</Form.Label>
+          <Form.Control
+            id="origin"
+            required
+            type="text"
+            placeholder="Japan"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Notes</Form.Label>
-          <Form.Control as="textarea" rows={3}></Form.Control>
+          <Form.Label>Notes:</Form.Label>
+          <Form.Control
+            id="notes"
+            required
+            as="textarea"
+            rows={3}
+            placeholder="A classic simple diver"
+            onChange={(e) => handleInputs(e)}
+          ></Form.Control>
         </Form.Group>
-        <Button variant="primary" type="submit" className="p-1">
-          Upload
-        </Button>
+        <div className="m-2 d-flex justify-content-center">
+          <Button variant="primary" type="submit">
+            Upload
+          </Button>
+        </div>
       </Form>
     </>
   );
